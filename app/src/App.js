d09bcb34fc1ceb75ece10b3dea6dd86cfc25254f
zorro200 <loren2018loren@gmail.com>
2022-04-30 21:03:52 +0200

    Add specific coordinates to Maps

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,23 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { currCoords } from './reducers/locationReducer.js'
 import Maps from './components/Maps'
 
 const App = () => {
-  return <Maps />
+  const dispatch = useDispatch()
+
+  const coords = useSelector((state) => state.location.currCoords)
+
+  useEffect(() => {
+    dispatch(currCoords())
+  }, [dispatch])
+
+  return (
+    coords
+      ? <Maps coords={coords} />
+      : null
+  )
 }
 
 export default App
