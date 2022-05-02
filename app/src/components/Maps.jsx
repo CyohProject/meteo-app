42fe8a3e04c7e12870631a80c8af8ed890f25a95
zorro200 <loren2018loren@gmail.com>
2022-05-02 18:14:51 +0200

    Update default map

--- app/src/components/Maps.jsx
+++ app/src/components/Maps.jsx
@@ -1,5 +1,5 @@
 export default function Maps ({ coords }) {
-  const src = `https://embed.windy.com/embed2.html?lat=${coords.lat}&lon=${coords.lon}&zoom=9&level=surface&overlay=temp&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1`
+  const src = `https://embed.windy.com/embed2.html?lat=${coords.lat}&lon=${coords.lon}&zoom=9&level=surface&overlay=rain&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1`
 
   return (
     <iframe
