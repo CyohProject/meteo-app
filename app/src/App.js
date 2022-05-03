c59ea3f1163f797a0da413de4d73742ffa37f488
zorro200 <loren2018loren@gmail.com>
2022-05-03 15:14:12 +0200

    Add MainPage sample & Add Menu & MainPage Link with img to App

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import { currCoords } from './reducers/locationReducer.js'
+import logo from './assets/logo.png'
+import Menu from './components/Menu'
+import MainPage from './components/MainPage'
 import Maps from './components/Maps'
 
 const App = () => {
@@ -17,11 +20,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <header>
-        <Link to='/' className='App-link'> Home </Link>
-        <Link to='/maps' className='App-link'> Maps </Link>
+        <Link id='logo' to='/main'><img src={logo} alt='logo' /></Link>
+        <Menu />
       </header>
       <Routes>
-        {/* <Route path='/' element={<MainPage />} /> */}
+        <Route path='/main' element={<MainPage />} />
         <Route path='/maps' element={coords ? <Maps coords={coords} /> : null} />
       </Routes>
     </BrowserRouter>
---
+++ app/src/components/MainPage.jsx
@@ -0,0 +1,3 @@
+const MainPage = () => <h1>Main - Meto and others :3</h1>
+
+export default MainPage
