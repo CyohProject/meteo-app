aa419708f3e4fe29df41a0a3667b19949443fc52
zorro200 <loren2018loren@gmail.com>
2022-05-06 15:31:49 +0200

    Add show a message if there are no alerts

--- app/src/components/Alerts.jsx
+++ app/src/components/Alerts.jsx
@@ -1,4 +1,4 @@
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { FiAlertTriangle } from 'react-icons/fi'
 import { BsInfoCircle } from 'react-icons/bs'
 import moment from 'moment'
@@ -11,18 +11,11 @@ import 'moment/locale/es'
 */
 
 export default function Alerts () {
-  // const alerts = useSelector(state => state.location.currMeteoData.alerts)
-  const alerts = [
-    {
-      event: 'Heat Advisory',
-      start: 1597341600,
-      end: 1597366800,
-      description: '...HEAT ADVISORY REMAINS IN EFFECT FROM 1 PM THIS AFTERNOON TO\n8 PM CDT THIS EVENING...\n* WHAT...Heat index values of 105 to 109 degrees expected.\n* WHERE...Creek, Okfuskee, Okmulgee, McIntosh, Pittsburg,\nLatimer, Pushmataha, and Choctaw Counties.\n* WHEN...From 1 PM to 8 PM CDT Thursday.\n* IMPACTS...The combination of hot temperatures and high\nhumidity will combine to create a dangerous situation in which\nheat illnesses are possible.',
-      tags: [
-        'Extreme temperature value'
-      ]
-    }
-  ]
+  const alerts = useSelector(state => (state.location.alerts))
+
+  const noAlerts = () => {
+    return <p id='no-alerts'>Actualmente no hay alertas meteorológicas en tu localización</p>
+  }
 
   const showAlerts = () => {
     return alerts.map((alert, index) => {
@@ -53,7 +46,7 @@ export default function Alerts () {
         Alertas
       </h1>
       <ul>
-        {showAlerts()}
+        {!alerts ? noAlerts() : showAlerts()}
       </ul>
     </>
   )
