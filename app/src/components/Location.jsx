2c0fc20dbcf58c55368a06b3a8dd76585a800939
zorro200 <loren2018loren@gmail.com>
2022-05-06 21:25:33 +0200

    Add dispatch locInfoAll manually & TODO

--- app/src/components/Location.jsx
+++ app/src/components/Location.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { obtainLocInfoAllManual } from '../reducers/locationReducer.js'
 
+/**
+ * TODO
+ * Mapa Google Maps -> Clicar en cualquier parte obtiene coordenadas y posterior info
+ * Botón para obtener ubicación y datos actuales
+ * Si hay un usuario, guardar el nombre de la localización en el usuario
+ * Style
+ * Agregar url del proyecto desplegado a las URL que pueden usar Google Maps API (en plataforma Google)
+ */
 export default function Location () {
   const [locName, setLocName] = useState('')
-
+  const dispatch = useDispatch()
   const handleLoc = (e) => {
     e.preventDefault()
 
+    dispatch(obtainLocInfoAllManual(locName))
     setLocName('')
   }
 
