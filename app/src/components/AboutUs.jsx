46f8e0d2767a5a5af16eb6cf046c38bc3861bb5b
Oriol-S <oriolsalvadoralcon@gmail.com>
2022-05-12 19:20:08 +0200

    Added footer with GitHub link

--- app/src/components/AboutUs.jsx
+++ app/src/components/AboutUs.jsx
@@ -1,16 +1,28 @@
 import '../styles/AboutUs.css'
+import githubIcon from '../assets/githubLogo.png'
 
 /**
- * Añadir GH
  * Añadir links de APIs y/o las cosas más
  * importantes que hayamos usado en el footer
  * Style
  */
+
 export default function AboutUs () {
   return (
     <>
-      <h1>Sobre Nosotros</h1>
-      <footer />
+      <div id='containerAboutUs'>
+        <h1>Sobre Nosotros</h1><br />
+        <p>Este es un proyecto de fin de estudios de Grado Superior en el IES Badia.</p>
+      </div>
+      <footer>
+        <a id='githubIcon' href='https://github.com/CyohProject/meteo-app'><img src={githubIcon} alt='githubIcon' /></a>
+        <a id='githubLink' href='https://github.com/CyohProject/meteo-app'>Github</a>
+        {/* <div id='footerText'>
+          <p>Lorenzo García</p>
+          <p>Oriol Salvador</p>
+          <p>Jose María Molero</p>
+        </div> */}
+      </footer>
     </>
   )
 }
