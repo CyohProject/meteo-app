01f58ddcc67fa31dff6a2d8846b5620d7976a6c9
jslworks <88553869+jslworks@users.noreply.github.com>
2022-05-13 15:53:12 +0200

    Uncomment Comments component. Now works

--- app/src/components/MainPage.jsx
+++ app/src/components/MainPage.jsx
@@ -1,11 +1,10 @@
 import MeteoArea from './MeteoSection/MeteoArea'
-// import Comments from './Comments'
+import Comments from './Comments'
 const { Box, Grid } = require('@mui/material')
 
 export default function MainPage (props) {
   return (
     <>
-
       <Grid container>
         <Grid item xs={6}>
           <Box
@@ -18,10 +17,7 @@ export default function MainPage (props) {
           <Box
             sx={{ bgcolor: '#F2E1EE' }}
           >
-            {/*
             <Comments />
-            Hay que adaptar rutas, entre App y el directorio actual
-            hay un salto. Css, por ejemplo, no va aquí */}
           </Box>
         </Grid>
       </Grid>
