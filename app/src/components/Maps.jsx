884092260d0eb13893dc82a2a19b94d2903296fe
zorro200 <loren2018loren@gmail.com>
2022-05-15 18:31:09 +0200

    Add comments & Update src method

--- app/src/components/Maps.jsx
+++ app/src/components/Maps.jsx
@@ -1,10 +1,15 @@
+/**
+ * @export Maps Weather maps external widget
+ * @param {*} { coords }
+ * @return {*} Iframe showing the weather maps
+ */
 export default function Maps ({ coords }) {
-  const src = `https://embed.windy.com/embed2.html?lat=${coords.lat}&lon=${coords.lon}&zoom=9&level=surface&overlay=rain&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1`
-
   return (
     <iframe
       title='maps'
-      src={src}
+      src={`https://embed.windy.com/embed2.html?lat=${coords.lat}&lon=${coords.lon}` +
+      '&zoom=9&level=surface&overlay=rain&product=ecmwf&menu=&message=true&marker=&calendar=now' +
+      '&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1'}
       width='100%' height='700px'
     />
   )
