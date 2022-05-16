505dcd165b8de53946e92296e119b150a13d3d4c
Oriol-S <oriolsalvadoralcon@gmail.com>
2022-05-16 15:50:06 +0200

    Add Comments to MainPage tabbar

--- app/src/components/MainPage.jsx
+++ app/src/components/MainPage.jsx
@@ -1,13 +1,15 @@
 import MeteoArea from './MeteoSection/MeteoArea'
 import Alerts from './Alerts'
+import Comments from './Comments'
 import { useState } from 'react'
 import TabPanel from './TabPanel'
 import { FiAlertTriangle } from 'react-icons/fi'
+import { GoComment } from 'react-icons/go'
 import '../styles/MainPage.css'
 const { Box, Grid, Tabs, Tab } = require('@mui/material')
 
 /** TODO
- * Añadir Comments al tabbar
+ *
  */
 export default function MainPage (props) {
   const [tab, setTab] = useState(0)
@@ -30,10 +32,14 @@ export default function MainPage (props) {
           <Box position='relative' sx={{ borderBottom: 1, borderColor: 'divider' }}>
             <Tabs value={tab} onChange={handleChange}>
               <Tab label={<span id='AlertsTab'><FiAlertTriangle color='red' /> Alerts </span>} />
+              <Tab label={<span id='CommentsTab'><GoComment color='blue' /> Comments </span>} />
             </Tabs>
           </Box>
           <TabPanel value={tab} index={0}>
             <Alerts />
+          </TabPanel>
+          <TabPanel value={tab} index={1}>
+            <Comments />
           </TabPanel>
         </Grid>
       </Grid>
