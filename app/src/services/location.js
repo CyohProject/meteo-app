79c44d8c2682f1e8f2cc5687f6331440afd3b1f1
zorro200 <loren2018loren@gmail.com>
2022-05-20 16:51:32 +0200

    Update backend url

--- app/src/services/location.js
+++ app/src/services/location.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/location'
+const baseUrl = 'https://meteo-app-frontend.herokuapp.com/api/location'
 
 /**
  * Get current user position from the browser
