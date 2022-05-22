a2fc1f54138108c62be2a5409763edc80955a20e
zorro200 <loren2018loren@gmail.com>
2022-05-23 00:44:02 +0200

    Add commentsReducer

--- app/src/store.js
+++ app/src/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { locationReducer } from './reducers/locationReducer.js'
+import { commentsReducer } from './reducers/commentsReducer.js'
 // Enables the store for ReduxDevTools on web browser
 import { composeWithDevTools } from 'redux-devtools-extension'
 // Redux middleware
 import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
-  location: locationReducer
+  location: locationReducer,
+  comments: commentsReducer
 })
 
 export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
